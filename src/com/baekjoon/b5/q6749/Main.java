712e22f4b5d0de5b413f6219ba5e652412eb0aaa
cares0 <cares00000@gmail.com>
2022-01-09 14:19:49 +0900

    q6749

---
+++ src/com/baekjoon/b5/q6749/Main.java
@@ -0,0 +1,17 @@
+package com.baekjoon.b5.q6749;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int youngest = sc.nextInt();
+		int middle = sc.nextInt();
+		
+		System.out.println(middle * 2 - youngest);
+		
+	}
+
+}
