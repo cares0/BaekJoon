d9670a506bf0ebc6d10ac5f497f418035d60e1e1
cares0 <cares00000@gmail.com>
2022-01-09 13:29:30 +0900

    q5338

---
+++ src/com/baekjoon/b5/q5338/Main.java
@@ -0,0 +1,14 @@
+package com.baekjoon.b5.q5338;
+
+public class Main {
+
+	public static void main(String[] args) {
+	       System.out.println("       _.-;;-._");
+	       System.out.println("'-..-'|   ||   |");
+	       System.out.println("'-..-'|_.-;;-._|");
+	       System.out.println("'-..-'|   ||   |");
+	       System.out.println("'-..-'|_.-''-._|");
+
+	}
+
+}
