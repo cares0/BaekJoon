ed54615c2517daf45cc5b26e8cde1e65d30503de
cares0 <cares00000@gmail.com>
2022-01-09 11:58:46 +0900

    q2914

---
+++ src/com/baekjoon/b5/q2914/Main.java
@@ -0,0 +1,19 @@
+package com.baekjoon.b5.q2914;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int i = sc.nextInt();
+		
+		int result = a * (i-1) + 1;
+		
+		System.out.println(result);
+		
+	}
+
+}
