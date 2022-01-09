3bd33472ebb99e60a9875b3ba80407a06c67e4d5
cares0 <cares00000@gmail.com>
2022-01-09 15:26:44 +0900

    q10757_BigInteger

---
+++ src/com/baekjoon/b5/q10757/Main.java
@@ -0,0 +1,18 @@
+package com.baekjoon.b5.q10757;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		
+		System.out.println(a.add(b));
+
+	}
+
+}
