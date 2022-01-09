3e6b100a68a2027f35d1d9ace05de4e40ad3d2b0
cares0 <cares00000@gmail.com>
2022-01-09 13:32:22 +0900

    q5339

---
+++ src/com/baekjoon/b5/q5339/Main.java
@@ -0,0 +1,19 @@
+package com.baekjoon.b5.q5339;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+
+	}
+
+}
