84a7787b93f9854204e2cc5b4b47d82e8f672e62
cares0 <cares00000@gmail.com>
2022-01-09 13:21:43 +0900

    q3046

---
+++ src/com/baekjoon/b5/q3046/Main.java
@@ -0,0 +1,17 @@
+package com.baekjoon.b5.q3046;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int r1 = sc.nextInt();
+		int avg = sc.nextInt();
+		
+		System.out.println(2*avg - r1);
+
+	}
+
+}
