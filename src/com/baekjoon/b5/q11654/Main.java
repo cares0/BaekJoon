4baebe448ecc05df25c07ba1ea96f0349b175eb4
cares0 <cares00000@gmail.com>
2022-01-09 15:44:16 +0900

    q11654_charAt()

---
+++ src/com/baekjoon/b5/q11654/Main.java
@@ -0,0 +1,18 @@
+package com.baekjoon.b5.q11654;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.next();
+		
+		int a = (int) input.charAt(0);
+		
+		System.out.println(a);
+
+	}
+
+}
